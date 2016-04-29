559fa38a45a0e6aafef53879e375cd75099ac159
greapflute <greapflutea2@yahoo.co.jp>
2016-04-29 22:39:31 +0900

    スクロール実装

--- practice2/main.js
+++ practice2/main.js
@@ -11,5 +11,22 @@ $(function() {
 	$('#search_tool_off').hide();
 	$('.search_form').hide();
     });
+
+    $(".btn_sample").click(function () {
+        var i = $(".btn_sample").index(this)
+        var p = $(".content").eq(i).offset().top;
+        $('html,body').animate({ scrollTop: p }, 'fast');
+        return false;
+    });
+ 
+    //ページ上部へ戻る
+    $(".btn_top").click(function () {
+        $('html,body').animate({ scrollTop: 0 }, 1500);
+        return false;
+    });
+    $(".btn_bottom").click(function () {
+        $('html,body').animate({ scrollTop: $(document).height() }, 1500);
+        return false;
+    });
 });
 
